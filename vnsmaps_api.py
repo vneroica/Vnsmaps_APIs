ceb5d84c49a379f9f2a8ad177a9449304d2f0c7f
vneroica <vneroica@gmail.com>
2020-09-16 16:41:17 +0700

    third commit

--- vnsmaps_api.py
+++ vnsmaps_api.py
@@ -6,7 +6,7 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/Vnsmaps"
 mongo = PyMongo(app)
 
 @app.route('/init')
@@ -14,15 +14,15 @@ def device_init():
     return jsonify({"status":"OK"})
 
 @app.route('/sendgps')
-def device_init():
+def sendgps():
     return jsonify({"status":"OK"})
 
 @app.route('/sendgrcode')
-def device_init():
+def sendqrcode():
     return jsonify({"status":"OK"})
 
 @app.route('/sendweight')
-def device_init():
+def sendweight():
     return jsonify({"status":"OK"})
 
 
