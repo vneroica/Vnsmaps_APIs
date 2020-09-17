22a4dcb67bf62ed6e853f8dea6e85e9fa48d15dd
vneroica <vneroica@gmail.com>
2020-09-17 23:40:46 +0700

    new commit

--- vnsmaps_api.py
+++ vnsmaps_api.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
+import datetime
 
 from flask_pymongo import PyMongo
 
@@ -13,19 +14,27 @@ mongo = PyMongo(app)
 @app.route('/init', methods=['POST'])
 def device_init():
     devices_db = mongo.db.devices
-    print (request.form['device_id'])
-    device_id = int(request.form['device_id'])
-    device = devices_db.find_one({'device_id': device_id})
-
-    if device:
-        if device['status'] == 0:
-            device['status'] = 1
-            devices_db.save(device)
-            return jsonify ({"status":"ok"}) , 200
-        else:
-            return jsonify ({"status":"fail","details":"device_already_activated"})
+    logs = mongo.db.logs
+    if not "device_id" in request.form:
+        logs.insert_one({"time": datetime.datetime.now(), "api":"init", "status":"fail"})
+        return jsonify({"status":"fail", "detail":"data_incorrect"}), 419
     else:
-        return jsonify ({"status":"fail","details":"device_id_incorrect"}), 201
+        
+        device_id = int(request.form['device_id'])
+        device = devices_db.find_one({'device_id': device_id})
+
+        if device:
+            if device['status'] == 0:
+                device['status'] = 1
+                devices_db.save(device)
+                logs.insert_one({"time": datetime.datetime.now(), "api":"init", "status":"ok"})
+                return jsonify ({"status":"ok"}) , 200
+            else:
+                logs.insert_one({"time": datetime.datetime.now(), "api":"init", "status":"fail"})
+                return jsonify ({"status":"fail","details":"device_already_activated"})
+        else:
+            logs.insert_one({"time": datetime.datetime.now(), "api":"init", "status":"fail"})
+            return jsonify ({"status":"fail","details":"device_id_incorrect"}), 422
 
 @app.route('/sendgps', methods=['POST'])
 def sendgps():
@@ -41,6 +50,6 @@ def sendweight():
 
 
 if __name__ == '__main__':
-   app.run()
+   app.run(debug=True)
 
 
