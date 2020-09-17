656f82c99154e5129b6d4c1b77c489443cbab82f
vneroica <vneroica@gmail.com>
2020-09-18 00:35:15 +0700

    new commit

--- vnsmaps_api.py
+++ vnsmaps_api.py
@@ -43,29 +43,15 @@ def device_init():
 def sendgps():
     devices_db = mongo.db.Gpsdata
     logs = mongo.db.logs
-    if not "device_id" and "long" and "lat" and "speed" and "time" in request.form:
-        logs.insert_one({"time": datetime.datetime.now(), "api":"init", "status":"fail"})
+    if not all(key in request.form for key in ('device_id', 'long', 'lat', 'speed', 'odbII', 'time')):
+        logs.insert_one({"time": datetime.datetime.now(), "api":"sendgps", "status":"fail"})
         return jsonify({"status":"fail", "detail":"bad_data"}), 419
     else:
         
-        device_id = int(request.form['device_id'])
-        print (device_id)
-        device = devices_db.find_one({'device_id': device_id})
-
-        print (device)
-
-        if device:
-            if device['status'] == 0:
-                device['status'] = 1
-                devices_db.save(device)
-                logs.insert_one({"time": datetime.datetime.now(), "api":"init", "status":"ok"})
-                return jsonify ({"status":"ok"}) , 200
-            else:
-                logs.insert_one({"time": datetime.datetime.now(), "api":"init", "status":"fail"})
-                return jsonify ({"status":"fail","details":"device_already_activated"})
-        else:
-            logs.insert_one({"time": datetime.datetime.now(), "api":"init", "status":"fail"})
-            return jsonify ({"status":"fail","details":"device_id_incorrect"}), 422
+        devices_db.insert_one({"device_id": request.form['device_id'], "long": request.form['long'], "lat"
+                               : request.form['lat'], "speed": request.form['speed'], "odbII": request.form['odbII']})
+        logs.insert_one({"time": datetime.datetime.now(), "api":"sendgps", "status":"ok"})
+        return jsonify ({"status":"ok"}) , 200
 
 @app.route('/sendgrcode', methods=['POST'])
 def sendqrcode():
