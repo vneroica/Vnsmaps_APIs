f1a4244ef1b51e1ca96dee9bbea5ae70be123653
vneroica <vneroica@gmail.com>
2020-09-18 00:45:43 +0700

    new commit

--- vnsmaps_api.py
+++ vnsmaps_api.py
@@ -41,73 +41,48 @@ def device_init():
 
 @app.route('/sendgps', methods=['POST'])
 def sendgps():
-    devices_db = mongo.db.Gpsdata
+    gps_db = mongo.db.Gpsdata
     logs = mongo.db.logs
     if not all(key in request.form for key in ('device_id', 'long', 'lat', 'speed', 'odbII', 'time')):
         logs.insert_one({"time": datetime.datetime.now(), "api":"sendgps", "status":"fail"})
         return jsonify({"status":"fail", "detail":"bad_data"}), 419
     else:
         
-        devices_db.insert_one({"device_id": request.form['device_id'], "long": request.form['long'], "lat"
-                               : request.form['lat'], "speed": request.form['speed'], "odbII": request.form['odbII']})
+        gps_db.insert_one({"device_id": request.form['device_id'], "long": request.form['long'], "lat"
+                               : request.form['lat'], "speed": request.form['speed'], "odbII": request.form['odbII'],
+                           "server_time": datetime.datetime.now()})
         logs.insert_one({"time": datetime.datetime.now(), "api":"sendgps", "status":"ok"})
         return jsonify ({"status":"ok"}) , 200
 
 @app.route('/sendgrcode', methods=['POST'])
 def sendqrcode():
-    devices_db = mongo.db.Devices
+    qrcode_db = mongo.db.Driverlicencedata
     logs = mongo.db.logs
-    if not "device_id" in request.form:
-        logs.insert_one({"time": datetime.datetime.now(), "api":"init", "status":"fail"})
-        return jsonify({"status":"fail", "detail":"data_incorrect"}), 419
+    if not all(key in request.form for key in ('device_id', 'long', 'lat', 'speed', 'odbII', 'time')):
+        logs.insert_one({"time": datetime.datetime.now(), "api":"sendgps", "status":"fail"})
+        return jsonify({"status":"fail", "detail":"bad_data"}), 419
     else:
         
-        device_id = int(request.form['device_id'])
-        print (device_id)
-        device = devices_db.find_one({'device_id': device_id})
-
-        print (device)
-
-        if device:
-            if device['status'] == 0:
-                device['status'] = 1
-                devices_db.save(device)
-                logs.insert_one({"time": datetime.datetime.now(), "api":"init", "status":"ok"})
-                return jsonify ({"status":"ok"}) , 200
-            else:
-                logs.insert_one({"time": datetime.datetime.now(), "api":"init", "status":"fail"})
-                return jsonify ({"status":"fail","details":"device_already_activated"})
-        else:
-            logs.insert_one({"time": datetime.datetime.now(), "api":"init", "status":"fail"})
-            return jsonify ({"status":"fail","details":"device_id_incorrect"}), 422
+        qrcode_db.insert_one({"device_id": request.form['device_id'], "long": request.form['long'], "lat"
+                               : request.form['lat'], "speed": request.form['speed'], "odbII": request.form['odbII'],
+                           "server_time": datetime.datetime.now()})
+        logs.insert_one({"time": datetime.datetime.now(), "api":"sendgps", "status":"ok"})
+        return jsonify ({"status":"ok"}) , 200
 
 @app.route('/sendweight', methods=['POST'])
 def sendweight():
-    devices_db = mongo.db.Devices
+    weight_db = mongo.db.weightdata
     logs = mongo.db.logs
-    if not "device_id" in request.form:
-        logs.insert_one({"time": datetime.datetime.now(), "api":"init", "status":"fail"})
-        return jsonify({"status":"fail", "detail":"data_incorrect"}), 419
+    if not all(key in request.form for key in ('device_id', 'long', 'lat', 'speed', 'odbII', 'time')):
+        logs.insert_one({"time": datetime.datetime.now(), "api":"sendgps", "status":"fail"})
+        return jsonify({"status":"fail", "detail":"bad_data"}), 419
     else:
         
-        device_id = int(request.form['device_id'])
-        print (device_id)
-        device = devices_db.find_one({'device_id': device_id})
-
-        print (device)
-
-        if device:
-            if device['status'] == 0:
-                device['status'] = 1
-                devices_db.save(device)
-                logs.insert_one({"time": datetime.datetime.now(), "api":"init", "status":"ok"})
-                return jsonify ({"status":"ok"}) , 200
-            else:
-                logs.insert_one({"time": datetime.datetime.now(), "api":"init", "status":"fail"})
-                return jsonify ({"status":"fail","details":"device_already_activated"})
-        else:
-            logs.insert_one({"time": datetime.datetime.now(), "api":"init", "status":"fail"})
-            return jsonify ({"status":"fail","details":"device_id_incorrect"}), 422
+        weight_db.insert_one({"device_id": request.form['device_id'], "long": request.form['long'], "lat"
+                               : request.form['lat'], "speed": request.form['speed'], "odbII": request.form['odbII'],
+                           "server_time": datetime.datetime.now()})
+        logs.insert_one({"time": datetime.datetime.now(), "api":"sendgps", "status":"ok"})
+        return jsonify ({"status":"ok"}) , 200
 
 
 if __name__ == '__main__':
